b5379173e56aea8316c6407e9a48eebdabc696e2
Satish-ambati <satish.ambati0804@gmail.com>
2025-09-15 09:52:53 +0530

    basic structure

---
+++ app/(tabs)/App.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-blue-500 text-xl font-bold">
+        Hello Tailwind in Expo!
+      </Text>
+    </View>
+  );
+}
---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,6 @@
+import { SafeAreaViewBase} from 'react-native';
+import App from './App';
+export default function HomeScreen() {
+  return (
+      <App/>
+  )}
